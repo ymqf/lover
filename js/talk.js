2fdfc2b0f45e23b8a038a9a7de5b1feefca2cc93
倚梦清风 <78645176+ymqf@users.noreply.github.com>
2021-08-26 12:27:43 +0800

    Add files via upload

---
+++ js/talk.js
@@ -0,0 +1,20 @@
+new TypeIt("#tqyxhj", {
+        loop: true,
+        cursorSpeed: 1000,
+        speed: 100
+    })
+    .type("ljs ❤ qj")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("一起走过的..LJS❤QJ")
+    .pause(3000)
+    .go();
+
+new TypeIt('#talkToXHJ', {
+    lifeLike: true,
+    cursorSpeed: 1000,
+    waitUntilVisible: true,
+    speed: 100
+}).go();
